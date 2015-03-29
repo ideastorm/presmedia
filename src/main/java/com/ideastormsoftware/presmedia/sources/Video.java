5afce19fdc788cc6a519be293088c23ba16d8280
Phil Hayward <philhayward@ideastormsoftware.com>
2015-03-28 21:04:43 -0600

    Fix imports

--- src/main/java/com/ideastormsoftware/presmedia/sources/Video.java
+++ src/main/java/com/ideastormsoftware/presmedia/sources/Video.java
@@ -5,13 +5,10 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import org.jcodec.api.JCodecException;
 
 public class Video extends ImageSource {
 
